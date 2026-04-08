9d4b1ec9954989b76ca27809222538a74bc48013
jameslbenjamin87 <james.l.benjamin87@gmail.com>
2026-04-07 20:02:37 -0600

    Update TaxIncome.java

--- src/PP03/TaxIncome.java
+++ src/PP03/TaxIncome.java
@@ -1,9 +1,28 @@
 package PP03;
 
-public class TaxIncome {
+public class TaxIncome implements Taxable{
+
+	@Override
+	public double compStateTax(double grossPay) {
+		// TODO Auto-generated method stub
+		return grossPay * STATE_TAX;
+	}
+
+	@Override
+	public double compFederalTax(double grossPay) {
+		// TODO Auto-generated method stub
+		return grossPay * FEDERAL_TAX;
+	}
+
+	@Override
+	public double compIncomeTax(double grossPay) {
+		// TODO Auto-generated method stub
+		return compStateTax(grossPay) + compFederalTax(grossPay);
+	}
 
 	// 1- this class implements the Taxable interface
 	// 2- implement all the unimplemented abstract methods in the Taxable Interface, income tax is computed based on state and federal taxes   
 	
 
 }
+
