ce31294529962ecb0882c74b720e3c54ba0b2df5
jameslbenjamin87 <james.l.benjamin87@gmail.com>
2026-04-07 19:54:25 -0600

    Update Employee.java

--- src/PP03/Employee.java
+++ src/PP03/Employee.java
@@ -11,5 +11,24 @@ public class Employee {
    // 3- add setters/getters methods
    // 4- add override toString() method that overrides toString() in the superclass Person 
 
+	public Employee(int eID, String fName, String lName, Address addr, Status empStatus) {
+        super(fName, lName, addr);
+        this.eID = eID;
+        this.empStatus = empStatus;
+    }
+
+    public Status getEmpStatus() {
+        return empStatus;
+    }
+
+    public int getEID() {
+        return eID;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee ID: " + eID + ", Name: " + super.toString() + ", Status: " + empStatus;
+    }
+
 	
 }
