fb10ee1919c0e0e3d0951a58b595752f2797de06
jameslbenjamin87 <james.l.benjamin87@gmail.com>
2026-04-07 19:53:33 -0600

    Update PayPeriod.java

--- src/PP03/PayPeriod.java
+++ src/PP03/PayPeriod.java
@@ -11,6 +11,17 @@ public class PayPeriod {
     // 1- add the class constructor
     // 2- add the setters/getters methods
     // 3- add override method toString() 
+
+	public PayPeriod(int pID, Date start, Date end) {
+        this.pID = pID;
+        this.pStartDate = start;
+        this.pEndDate = end;
+    }
+
+    @Override
+    public String toString() {
+        return "Period ID: " + pID + ", Start: " + pStartDate + ", End: " + pEndDate;
+    }
     
 
 	
