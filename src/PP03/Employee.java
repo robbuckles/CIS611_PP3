d5ea2e7eb95809d69e417ffc6cf176c110d6acaf
jameslbenjamin87 <james.l.benjamin87@gmail.com>
2026-04-10 14:16:01 -0600

    Update Employee.java

--- src/PP03/Employee.java
+++ src/PP03/Employee.java
@@ -10,8 +10,7 @@ public class Employee extends Person {
     // 2- add the subclass Employee constructor that calls the supper Person class constructor, you should provide input data for all parent class data fields
    // 3- add setters/getters methods
    // 4- add override toString() method that overrides toString() in the superclass Person 
-
-	public Employee(int eID, String fName, String lName, Address addr, Status empStatus) {
+    public Employee(int eID, String fName, String lName, Address addr, Status empStatus) {
         super(fName, lName, addr);
         this.eID = eID;
         this.empStatus = empStatus;
@@ -27,8 +26,10 @@ public class Employee extends Person {
 
     @Override
     public String toString() {
-        return "Employee ID: " + eID + ", Name: " + super.toString() + ", Status: " + empStatus;
+        return "Employee ID: " + eID +
+               "\nName: " + getfName() + " " + getlName() +
+               "\nStatus: " + empStatus +
+               "\nAddress: " + getAddress();
     }
-
 	
 }
