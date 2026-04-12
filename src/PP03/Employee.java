a81c5458944874dac0487326f20fe1ea5408c5d6
jameslbenjamin87 <james.l.benjamin87@gmail.com>
2026-04-12 11:02:54 -0600

    Final Employee.java

--- src/PP03/Employee.java
+++ src/PP03/Employee.java
@@ -1,3 +1,18 @@
+//*********************************************************************
+//*  
+//*         CIS611 Spring 2026 James Benjamin & Rob Buckles  
+//*  
+//*              Programming project  PP03  
+//*  
+//*          This class is the basic Employee handling class.  
+//*          
+//*  
+//*  
+//*                  12 APR 2026  
+//*  
+//*              Saved in: Employee.java  
+//*  
+//*********************************************************************
 package PP03;
 
 
