0ff1ee2601843122181ac6ee46e4fd8b1e4ab6e6
jameslbenjamin87 <james.l.benjamin87@gmail.com>
2026-04-12 11:01:16 -0600

    Final Status.java

--- src/PP03/Status.java
+++ src/PP03/Status.java
@@ -1,7 +1,22 @@
+//*********************************************************************
+//*  
+//*         CIS611 Spring 2026 James Benjamin & Rob Buckles  
+//*  
+//*              Programming project  PP03  
+//*  
+//*          This class is static Status options.  
+//*          
+//*  
+//*  
+//*                  12 APR 2026  
+//*  
+//*              Saved in: Status.java  
+//*  
+//*********************************************************************
 package PP03;
 
 public enum Status {
 	
 	FULLTIME, HOURLY
-
+	//changed to all caps 
 }
