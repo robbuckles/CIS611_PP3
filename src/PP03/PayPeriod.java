703d4ab6e2ea40f86ef6deefc3c65333c64b90c4
jameslbenjamin87 <james.l.benjamin87@gmail.com>
2026-04-12 11:02:36 -0600

    Final PayPeriod.java

--- src/PP03/PayPeriod.java
+++ src/PP03/PayPeriod.java
@@ -1,3 +1,18 @@
+//*********************************************************************
+//*  
+//*         CIS611 Spring 2026 James Benjamin & Rob Buckles  
+//*  
+//*              Programming project  PP03  
+//*  
+//*          This class is the Pay Period collector.  
+//*          
+//*  
+//*  
+//*                  12 APR 2026  
+//*  
+//*              Saved in: PayPeriod.java  
+//*  
+//*********************************************************************
 package PP03;
 
 import java.util.Date;
